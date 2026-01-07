51989f811e4825786137a5e66d4f5a79def62ac6
Matheesha-Malshan <mamalshan112@gmail.com>
2026-01-07 11:10:07 +0530

    malshan

--- abc.java
+++ abc.java
@@ -1,13 +1,11 @@
 class Main{
 
-	
+
 	
 	public static void main(String [] args){
 		
 		System.out.println("matheesha malshan");
 	}
 	
-	public static void name(){
-		System.out.println("matheesha malshan");
-	}
+	
 }
